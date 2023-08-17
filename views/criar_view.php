302bef9829d38417be24493e9297982db365fb3c
PedroPGPR <pedrofcporto336@gmail.com>
2023-08-17 16:40:15 +0100

    Iniciar a Criação da Página para criar novos contactos

---
+++ views/criar_view.php
@@ -0,0 +1,9 @@
+<main class="container">
+    <div class="row">
+        <div class="col-9">
+            <form action="" method="post">
+                
+            </form>
+        </div>
+    </div>
+</main>
--- views/home_view.php
+++ views/home_view.php
@@ -1,22 +1,4 @@
 <main class="d-flex justify-content-center align-items-center">
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Tem a certeza?</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    Irá apagar o contacto com o ID ( <?= $rotas[1]; ?> ) | Esta ação é permanente e não revertivel
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                    <button type="button" class="btn btn-danger">Apagar</button>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="container">
         <div class="row">
             <div class="col-9 mx-auto">
@@ -31,21 +13,14 @@
                     </tr>
                     <?php foreach($contactos as $c): ?>
                         <tr>
-                            <td><?= $c['id'] ?></td>
-                            <td><?= $c['name'] ?></td>
-                            <td><?= $c['phone'] ?></td>
-                            <td><?= $c['email'] ?></td>
+                            <td><?= $c['id']; ?></td>
+                            <td><?= $c['name']; ?></td>
+                            <td><?= $c['phone']; ?></td>
+                            <td><?= $c['email']; ?></td>
                             <td>
-                                <a href="#">
-                                    <i class="fa-regular fa-eye m-1" style="color: #1eff00;"></i>
-                                </a>
-                                <a href="#">
-                                    <i class="fa-regular fa-pen-to-square m-1" style="color: #004cff;"></i>
-                                </a>
-                                <a href="<?= ($url_base . $c['id']); ?>">
-                                    <!-- Button trigger modal -->
-                                    <i class="fa-solid fa-trash-can m-1" style="color: #ff0000;" data-bs-toggle="modal" data-bs-target="#exampleModal"></i>
-                                </a>
+                                <i class="fa-regular fa-eye m-1" style="color: #1eff00;"></i>
+                                <i class="fa-regular fa-pen-to-square m-1" style="color: #004cff;"></i>
+                                <i class="fa-solid fa-trash-can m-1" style="color: #ff0000;"></i>
                             </td>
                         </tr>
                     <?php endforeach; ?>
